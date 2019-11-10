34c51c2d92c76bb9002e03db7da9c7414e73d7d1
Omar <omarwsuleiman@gmail.com>
2019-11-10 19:49:36 +0000

    output to pgm file
    tried to implement the for loop but now the tests are getting stuck

--- cw_code/gol.go
+++ cw_code/gol.go
@@ -81,6 +81,12 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 		}
 	}
 
+	for y := range world {
+		for x := range world[y]{
+			d.io.outputVal <- world[y][x]
+		}
+	}
+
 	// Make sure that the Io has finished any output before exiting.
 	d.io.command <- ioCheckIdle
 	<-d.io.idle
