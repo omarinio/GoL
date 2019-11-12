cdf24d2d10ca0c5c6ba7f8ed052cb696ed85d2b2
Omar <omarwsuleiman@gmail.com>
2019-11-12 10:26:05 +0000

    Stage 1a 100% Complete
    .pgm file now actually output by program

--- cw_code/gol.go
+++ cw_code/gol.go
@@ -7,7 +7,7 @@ import (
 )
 
 func outputWorld(p golParams, d distributorChans, world [][]byte) {
-	d.io.command <- ioInput
+	d.io.command <- ioOutput
 	d.io.filename <- strings.Join([]string{strconv.Itoa(p.imageWidth), strconv.Itoa(p.imageHeight)}, "x")
 
 	// Sends the world to the pgm channel
@@ -18,30 +18,31 @@ func outputWorld(p golParams, d distributorChans, world [][]byte) {
 	}
 }
 
-func worker(startY, endY, startX, endX int, out chan<- byte) {
-    
-}
-func updateCell(p golParams, y int, x int, world [][]byte, world2 [][]byte) {
-	alive := 0
-	for i := -1; i <= 1 ; i++ {
-		for j := -1; j <= 1; j++ {
-			if (i != 0 || j != 0) && world2[((y+i)+p.imageHeight)%p.imageHeight][((x+j)+p.imageWidth)%p.imageWidth] != 0 {
-				alive++
+//func worker(startY, endY, startX, endX int, p golParams, out chan<- byte) {
+//    imagePart := updateCell(p, )
+//}
+
+//Logic for Game of Life
+func updateCell(p golParams, x, y int, world [][]byte, world2 [][]byte) {
+
+		alive := 0
+		for i := -1; i <= 1 ; i++ {
+			for j := -1; j <= 1; j++ {
+				if (i != 0 || j != 0) && world2[((y+i)+p.imageHeight)%p.imageHeight][((x+j)+p.imageWidth)%p.imageWidth] != 0 {
+					alive++
+				}
 			}
 		}
-	}
-	if world2[y][x] != 0 {
-		if alive < 2 || alive > 3 {
-			world[y][x] = world[y][x] ^ 0xFF
+		if world2[y][x] != 0 {
+			if alive < 2 || alive > 3 {
+				world[y][x] = world[y][x] ^ 0xFF
+			}
+		} else {
+			if alive == 3 {
+				world[y][x] = world[y][x] ^ 0xFF
+			}
 		}
-	} else {
-		if alive == 3 {
-			world[y][x] = world[y][x] ^ 0xFF
-		}
-	}
 }
-
-
 
 
 // distributor divides the work between workers and interacts with other goroutines.
@@ -68,18 +69,24 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 		}
 	}
 
+	//Copy of world
 	world2 := make([][]byte, len(world))
 	for i := range world {
 		world2[i] = make([]byte, len(world[i]))
 		copy(world2[i], world[i])
 	}
 
-	//Array of channels intended for workers
-	out := make([]chan byte, p.threads)
-
-	for i := range out {
-		out[i] = make(chan byte)
-	}
+	//workerHeight := p.imageHeight / p.threads
+	////Array of channels intended for workers
+	//out := make([]chan byte, p.threads)
+	//
+	//for i := range out {
+	//	out[i] = make(chan byte)
+	//}
+	//
+	//for i := 0; i < 4; i++ {
+	//	go worker(i*workerHeight, (i+1)*workerHeight, 0, p.imageWidth, p, out[i])
+	//}
 
 
 	// Calculate the new state of Game of Life after the given number of turns.
@@ -95,6 +102,8 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 			copy(world2[i], world[i])
 		}
 	}
+
+	outputWorld(p, d, world)
 
 	// Create an empty slice to store coordinates of cells that are still alive after p.turns are done.
 	var finalAlive []cell
@@ -113,6 +122,4 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 
 	// Return the coordinates of cells that are still alive.
 	alive <- finalAlive
-
-	outputWorld(p, d, world)
 }
